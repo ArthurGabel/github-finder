41c9f0f1fe82c7e585669ffac3a4f3613a87a9bf
Arthur Gäbel <arthurgabel@hotmail.com>
2023-01-17 08:11:50 -0300

    ui js controler added

---
+++ ui.js
@@ -0,0 +1,68 @@
+class Ui {
+  constructor() {
+    this.profile = document.getElementById('profile');
+  }
+
+  sowProfile(user) {
+    this.profile.innerHTML = `
+    <div class="card card-body mb-3">
+      <div class="row">
+        <div class="col-md-3">
+          <img class="img-fluid mb-2" src="${user.avatar_url}"></img>
+          <a href="${user.html_url}" target="_balnk" class="btn btn-primary btn-block mb-3">View Profile</a>
+        </div> 
+        <div class="col-md-9">
+          <span class="badge badge-primary mb-1">Public Repos: ${user.public_repos}</span>
+          <span class="badge badge-secondary mb-1">Public Gists: ${user.public_gists}</span>
+          <span class="badge badge-success mb-1">Followers: ${user.followers}</span>
+          <span class="badge badge-info mb-1">Following: ${user.following}</span>
+          <br><br>
+          <ul class="list-group">
+            <li class="list-group-item">Company: ${user.company}</li>
+            <li class="list-group-item">Website: ${user.blog}</li>
+            <li class="list-group-item">Location: ${user.location}</li>
+            <li class="list-group-item">Member since: ${user.created_at}</li>
+          </ul>
+        </div> 
+      </div> 
+    </div>
+    <h3 class="page-heading mb-3">Latest Repos</h3>
+    <div id="repos"></div>
+    `; 
+  }
+
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(repo => {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge badge-primary mb-1">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary mb-1">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success mb-1">Forks: ${repo.forks_count}</span>
+          </div>
+          </div>
+        </div>
+      `;
+    })
+
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  showAlert(message) {
+    this.profile.innerHTML = `
+    <div class="card mb-3">
+      <span class="badge badge-danger">${message}</span>
+    </div>
+    `;
+  }
+
+  clearProfile() {
+    this.profile.innerHTML = '';
+  }
+}
